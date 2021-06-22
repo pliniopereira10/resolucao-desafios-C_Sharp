8cdedbf124921366aa90867163630b953a1813ab
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-22 16:08:54 -0300

    Primeiro Commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá GitHub !!");
+            Console.Write("Estou animado com o primeiro programa na linguagem C# !!!");
+            Console.WriteLine(":-D");
+        }
+    }
+}
