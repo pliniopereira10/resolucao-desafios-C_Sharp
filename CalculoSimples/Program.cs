1033ebc6cc35a9c01d0943c9f837d30594903ec6
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-23 10:52:29 -0300

    Add atividade SalarioCom Bonus e CalculoSimples

---
+++ CalculoSimples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] informacaoUm;
+            string [] informacaoDois;
+            int codigoUm, codigoDois, quantidadeUm, quantidadeDois;
+            double valorUnitUm, valorUnitDois, valorTotal;
+
+            Console.WriteLine("Peça 1.");
+            Console.WriteLine("Digite o código, quantidade e valor da unidade:");
+            informacaoUm = Console.ReadLine().Split(' ');
+
+            codigoUm = int.Parse(informacaoUm [0]);
+            quantidadeUm = int.Parse(informacaoUm [1]);
+            valorUnitUm = double.Parse(informacaoUm [2],CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Peça 2.");
+            Console.WriteLine("Digite o código, quantidade e valor da unidade:");
+            informacaoDois = Console.ReadLine().Split(' ');
+
+            codigoDois = int.Parse(informacaoDois [0]);
+            quantidadeDois = int.Parse(informacaoDois [1]);
+            valorUnitDois = double.Parse(informacaoDois [2],CultureInfo.InvariantCulture);
+
+            valorTotal = quantidadeUm * valorUnitUm + quantidadeDois * valorUnitDois;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ SalarioComBonus/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioComBonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salarioFixo, salarioTotal, totalVendido;
+
+            Console.Write("Nome do vendedor: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Salário fixo: ");
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Total de vendas: ");
+            totalVendido = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salarioTotal = salarioFixo + (totalVendido * 0.15);
+
+            Console.WriteLine("TOTAL = R$ " + salarioTotal.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
