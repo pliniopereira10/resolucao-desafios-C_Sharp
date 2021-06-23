febf84a8460cd3ce6403734b240cd4bfcb3bf483
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-23 17:33:36 -0300

    Add da atividade MedidasDoRetangulo

---
+++ 1.EstruturaSequencial/MedidasDoRetangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿﻿using System;
+using System.Globalization;
+
+namespace ExRes002MedidasRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] valores;            
+            double altura, basi;
+            double area, perimetro, diagonal;
+            
+            Console.WriteLine("Informe a base e altura do retângulo:");
+            valores = Console.ReadLine().Split(' ');
+
+            altura = double.Parse(valores [0], CultureInfo.InvariantCulture);
+            basi = double.Parse(valores [1], CultureInfo.InvariantCulture);
+
+            area = basi * altura ;
+            perimetro = 2 * (basi + altura);
+            diagonal = Math.Sqrt(Math.Pow(basi, 2) + Math.Pow(altura, 2));
+
+            Console.WriteLine("AREA = " + area.ToString("F4"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F4"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Diagonal = " + diagonal.ToString("F4"), CultureInfo.InvariantCulture);
+
+        }
+    }
+}
