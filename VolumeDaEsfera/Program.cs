65e780afd969295209c703367b72b4feb35955fa
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-23 11:18:59 -0300

    Add da atividade VolumeDaEsfera

---
+++ VolumeDaEsfera/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace VolumeDaEsfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double volume;
+            double pi;
+            double raio;
+
+            pi = 3.14159;
+           
+            Console.Write("Digite o raio da esfera: ");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            raio = Math.Pow(raio, 3);
+            volume = (4.0 / 3) * pi * raio;
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
