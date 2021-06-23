a1e7ecf92565320d8a97820a0b8c746f1bcb9fb2
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-23 16:27:28 -0300

    Add atividade GastoDeCombustivel

---
+++ GastoDeCombustivel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GastoDeCombustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoGasto, velocidadeMedia, distanciaPercorrida;
+            double  mediaDeConsumo,totalConsumido;
+
+            mediaDeConsumo = 12;
+
+            Console.WriteLine("Qual tempo gasto na viagem ?");
+            tempoGasto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a velocidade média ?");
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+            distanciaPercorrida = tempoGasto * velocidadeMedia ;
+
+            totalConsumido = distanciaPercorrida / mediaDeConsumo;
+
+            Console.WriteLine("Total consumido: " + totalConsumido.ToString("F3"));
+
+
+        }
+    }
+}
