047a1106ee54ea11cc683ab39e0e18056b93175c
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-23 14:50:13 -0300

    Add dos exerc ConsumoCombustivel e DistanciaEntreDoisPontos

---
+++ ConsumoCombustivel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsumoCombustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanciaKm;
+            double totalConsumido, consumoMedio;
+
+            Console.Write("Digite a distância percorrida: Km ");
+            distanciaKm = int.Parse(Console.ReadLine());
+            Console.Write("Quantos litros consumido: L ");
+            totalConsumido = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumoMedio = distanciaKm / totalConsumido;
+
+            Console.WriteLine(consumoMedio.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+
+        }
+    }
+}
---
+++ DistanciaEntreDoisPontos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace DistanciaEntreDoisPontos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] calculoPontoUm;
+            string [] calculoPontoDois;
+            double pontoUmX, pontoUmY, calculoX;
+            double pontoDoisX, pontoDoisY, calculoY;
+            double distanciaXy, somaXy;
+
+            Console.WriteLine("Informe:");
+            Console.WriteLine("Valores de X e Y do ponto 1.");
+            calculoPontoUm = Console.ReadLine().Split(' ');
+
+            pontoUmX = double.Parse(calculoPontoUm [0], CultureInfo.InvariantCulture);
+            pontoUmY = double.Parse(calculoPontoUm [1], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe:");
+            Console.WriteLine("Valores de X e Y do ponto 2.");
+            calculoPontoDois = Console.ReadLine().Split(' ');
+
+            pontoDoisX = double.Parse(calculoPontoDois [0], CultureInfo.InvariantCulture);
+            pontoDoisY = double.Parse(calculoPontoDois [1], CultureInfo.InvariantCulture);
+
+
+            calculoX = Math.Pow(pontoDoisX - pontoUmX, 2.0);
+            calculoY = Math.Pow(pontoDoisY - pontoUmY, 2.0);
+
+            somaXy = calculoX + calculoY;
+
+            distanciaXy = Math.Sqrt(somaXy);
+
+            Console.WriteLine("A distância entre os pontos é " + distanciaXy.ToString("F4", CultureInfo.InvariantCulture));
+            
+            
+        }
+    }
+}
