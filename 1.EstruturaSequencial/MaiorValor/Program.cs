768da57af09dde889b333dbea3cbce47113efe69
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-23 18:54:20 -0300

    Refatoração do código na pasta MaiorValor

--- 1.EstruturaSequencial/MaiorValor/Program.cs
+++ 1.EstruturaSequencial/MaiorValor/Program.cs
@@ -6,7 +6,25 @@ namespace MaiorValor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Formula: MaiorAB = (a + b + abs(a - b)) / 2
+            string [] mensagem;
+            int valorA, valorB, valorC;
+            int maiorAB, maior;
+
+            Console.WriteLine("Informe 3 valores:");
+            mensagem = Console.ReadLine().Split(' ');
+
+            valorA = int.Parse(mensagem [0]);
+            valorB = int.Parse(mensagem [1]);
+            valorC = int.Parse(mensagem [2]);
+
+            maiorAB = (valorA + valorB + Math.Abs(valorA - valorB)) / 2;
+
+            maior = (maiorAB + valorC + Math.Abs(maiorAB - valorC)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+
+
         }
     }
 }
