499bbe9f83a60ac428f659996802308727eff4fc
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-22 21:41:17 -0300

    Add da atividade AreaDoCirculo

---
+++ AreaDoCirculo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaDoCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+            double constante = 3.14159;
+            
+            Console.Write("Digite o raio do círculo: ");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            raio = Math.Pow(raio, 2);
+
+            area = constante * raio;
+
+            Console.WriteLine("Área= " + area.ToString("F4", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
