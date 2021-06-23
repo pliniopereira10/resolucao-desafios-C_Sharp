9f203f52a54bb19eea8cc04792e6bde6e171a873
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-22 22:12:20 -0300

    Add Exercicíos Soma e Produto

---
+++ Produto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Produto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUm, valorDois, produto;
+
+            Console.Write("Digite um número: ");
+            valorUm =  int.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro número: ");
+            valorDois =  int.Parse(Console.ReadLine());
+
+            produto = valorUm * valorDois;
+
+            Console.WriteLine("PROD = " + produto);
+        }
+    }
+}
---
+++ Soma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorUm, valorDois, soma;
+
+            Console.Write("Digite primeiro número: ");
+            valorUm = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite segundo número: ");
+            valorDois = int.Parse(Console.ReadLine());
+
+            soma = valorUm + valorDois;
+
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
