9e008f68e114c1234cad50e04bf8cd4d54df0c1b
pliniopereira10 <plinio.pereira10@gmail.com>
2021-06-23 21:34:16 -0300

    Add atividade ConversaoDeTempo

---
+++ 1.EstruturaSequencial/ConversaoDeTempo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConversaoDeTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas, minutos, segundos;
+            int valor, resto;
+
+            Console.WriteLine("Informe os segundos:");
+            valor = int.Parse(Console.ReadLine());
+
+            horas = valor / 3600;
+            resto = valor % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
