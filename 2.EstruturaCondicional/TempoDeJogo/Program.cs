5fc158c96e492f5ad29acfb0e1ac7a0a0efc3d1c
pliniopereira10 <plinio.pereira10@gmail.com>
2021-07-13 21:09:24 -0300

    Adição da atividade TempoDeJogo

---
+++ 2.EstruturaCondicional/TempoDeJogo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TempoDeJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, horaFinal, tempoJogo;
+            String [] inicioFim;
+
+            Console.WriteLine("Digite a hora Inicial e final de uma jogo:");
+            inicioFim = Console.ReadLine().Split(' ');
+
+            horaInicial = int.Parse(inicioFim [0]);
+            horaFinal= int.Parse(inicioFim [1]);
+
+            if (horaInicial  < horaFinal ) {
+                tempoJogo = horaFinal - horaInicial;
+
+            } else {
+                tempoJogo = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + tempoJogo + " HORA(S)");
+
+
+
+
+
+
+
+
+        }
+    }
+}
