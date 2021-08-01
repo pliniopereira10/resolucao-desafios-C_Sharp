58d641d8df4162541c92b03005e0f7e3c7c8f8f9
pliniopereira10 <plinio.pereira10@gmail.com>
2021-07-31 22:12:48 -0300

    Adição do link e atividade SequenciaIJ2

---
+++ 3.EstruturaRepetitiva/SequenciaIJ2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Programa imprime sequência númerica dentro de laço For encadeado
+
+using System;
+
+namespace SequenciaIJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int letraI = 1; letraI <= 9; letraI+= 2)
+            {
+                for (int letraJ = 7; letraJ >= 5; letraJ--)
+                {
+                    Console.WriteLine("I=" + letraI + " J=" + letraJ);
+                }
+            }
+
+            
+            
+           
+            
+        }
+    }
+}
